e0ca35170aec3732602db319edcf58676acdd10b
jaywnz <64354555+jaywnz@users.noreply.github.com>
2021-05-07 15:51:15 +1200

    write stats to JSON file

--- src/java/nz/ac/massey/cs/webtech/s_20020003/server/stats.java
+++ src/java/nz/ac/massey/cs/webtech/s_20020003/server/stats.java
@@ -5,13 +5,23 @@
  */
 package nz.ac.massey.cs.webtech.s_20020003.server;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Reader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -27,36 +37,10 @@ public class stats extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * @throws org.json.simple.parser.ParseException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet stats</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet stats at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ParseException {
 
         // get existing session
         HttpSession session = request.getSession(false);
@@ -64,20 +48,6 @@ public class stats extends HttpServlet {
         if (session == null) {
             System.out.println("No session in progress.");
             response.setStatus(404);
-            response.setContentType("text/html;charset=UTF-8");
-            try (PrintWriter out = response.getWriter()) {
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Blackjack</title>");
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>404 Not Found</h1>");
-                out.println("<p>There is no currently active session.</p>");
-                out.println("<a href='/assignment2_server_20020003/jack/start'>Start a new game</a>.");
-                out.println("</body>");
-                out.println("</html>");
-            }
             return;
         }
 
@@ -85,6 +55,7 @@ public class stats extends HttpServlet {
         String t = (String) session.getAttribute("totalGames");
         String u = (String) session.getAttribute("userGamesWon");
 
+        // initialise if not already
         if (t == null) {
             t = "0";
         }
@@ -96,17 +67,7 @@ public class stats extends HttpServlet {
         int userGamesWon = Integer.parseInt(u);
         double winPercentage = 0.0;
 
-//                // increment user games won
-//        if (session.getAttribute("winner").equals("user")) {
-//            String wonStr = (String)session.getAttribute("userGamesWon");
-//            if (wonStr == null) {
-//                wonStr = "1";
-//            }
-//            int won = Integer.parseInt(wonStr);
-//            session.setAttribute("userGamesWon", String.valueOf(won+1));
-//        }
-//        int userGamesWon = (int)session.getAttribute("userGamesWon");
-//        int totalGames = (int)session.getAttribute("totalGames");
+        // FIXME -- not calculating stats correctly
         if (totalGames == 0) {
             response.setContentType("text/plain;charset=UTF-8");
             try (PrintWriter out = response.getWriter()) {
@@ -123,7 +84,56 @@ public class stats extends HttpServlet {
             out.println("Win percentage: " + winPercentage + "%");
         }
 
-//        processRequest(request, response);
+        // save stats to JSON
+        JSONObject stats = new JSONObject();
+        JSONArray data = new JSONArray();
+        data.add(totalGames);
+        data.add(userGamesWon);
+        data.add(winPercentage);
+
+        String sessionId = (String) session.getId();
+        stats.put(sessionId, data);
+        System.out.println(stats);
+
+        // TODO - only records data of latest session
+        File f = new File("C:\\Users\\Jay\\Desktop\\stats.json");
+        if (f.isFile()) {
+            try (Reader reader = new FileReader("C:\\Users\\Jay\\Desktop\\stats.json")) {
+                JSONParser parser = new JSONParser();
+                JSONObject obj = (JSONObject) parser.parse(reader);
+                obj.putAll(stats);
+                try (FileWriter file = new FileWriter("C:\\Users\\Jay\\Desktop\\stats.json")) {
+                    file.write(stats.toJSONString());
+                    file.flush();
+                    file.close();
+                }
+            }
+        } else {
+            try (FileWriter file = new FileWriter("C:\\Users\\Jay\\Desktop\\stats.json")) {
+                file.write(stats.toJSONString());
+                file.flush();
+                file.close();
+            }
+        }
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(stats.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -137,7 +147,11 @@ public class stats extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(stats.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
